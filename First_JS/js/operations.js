05db3fef698b6a69377c0ba21d1a69af915bf8a1
unknown <johnrixdomaoal6@gmail.com>
2025-02-11 11:09:13 +0800

    First JS

---
+++ First_JS/js/operations.js
@@ -0,0 +1,16 @@
+function sum(num1,num2){
+  const sum = num1 + num2;
+  console.log("The sum is ", sum);
+}
+function difference(num1,num2){
+    const sum = num1 - num2;
+    console.log("The difference is ", sum);
+  }
+  function product(num1,num2){
+    const sum = num1 * num2;
+    console.log("The product is ", sum);
+  }
+  function quotient(num1,num2){
+    const quo = num1 / num2;
+    console.log("The quotient is ", quo);
+  }
