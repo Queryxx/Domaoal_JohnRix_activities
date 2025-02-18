656791d95adf4fff4c508b8561b2ca6e2e314afa
unknown <johnrixdomaoal6@gmail.com>
2025-02-18 11:43:15 +0800

    with js

--- Guess-Number/js/guess.js
+++ Guess-Number/js/guess.js
@@ -1,10 +1,31 @@
-function checkguess(){
-    const guess = document.getElementById('guess').value;
-    const ans = document.getElementById('ans');
-    if (guess == 12) {
-      ans.innerHTML = "You got the correct answer";
+function checkNumber() {
+
+  const num = document.getElementById('guess').value;
+  tri--;
+  t++;
+  if (num == random) {
+    document.getElementById('message').innerHTML = "Correct";
+  }
+  else {
+    if (num < random) {
+      document.getElementById('message').innerHTML = "Higher";
     }
-    else {
-        ans.innerHTML = "Wrong Number";
+    else if (num > random) {
+      document.getElementById('message').innerHTML = "Lower";
     }
+  }
+  if (tri < 0) {
+
+    document.getElementById('tries').innerHTML = "You dont have remaining tries";
+    alert("You dont have remaining tries");
+  }
+  else {
+    document.getElementById('tries').innerHTML = "Tries " + tri;
+  }
+  if (num == random) {
+    tri++;
+    t--;
+    document.getElementById('tries').innerHTML = "Congrats, you tried " + t + " tries to guess";
+  }
+
 }
